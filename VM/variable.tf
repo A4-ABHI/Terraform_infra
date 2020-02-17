d3579295b3004a16aa8404e7b780dcd1cf9f312b
A4-ABHI <55126804+A4-ABHI@users.noreply.github.com>
2020-02-17 15:06:19 +0530

    Add files via upload

---
+++ VM/variable.tf
@@ -0,0 +1,6 @@
+variable "prefix" {
+  description = "The Prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which the resources in this example should exist"
