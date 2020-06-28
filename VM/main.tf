2bb8b69690c716094a155e61e74b726326d1b8be
A4-ABHI <55126804+A4-ABHI@users.noreply.github.com>
2020-06-29 03:30:24 +0530

    Update main.tf

--- VM/main.tf
+++ VM/main.tf
@@ -1,39 +1,88 @@
-resource "azurerm_virtual_machine" "example" {
-  name                  = "${var.prefix}-vm"
-  location              = "${azurerm_resource_group.example.location}"
-  resource_group_name   = "${azurerm_resource_group.example.name}"
-  network_interface_ids = ["${azurerm_network_interface.example.id}"]
-  vm_size               = "Standard_F2"
-
-  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
-  # NOTE: This may not be optimal in all cases.
-  delete_os_disk_on_termination = true
-
-  # This means the Data Disk Disk will be deleted when Terraform destroys the Virtual Machine
-  # NOTE: This may not be optimal in all cases.
-  delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+provider "azurerm" {
+version = "2.12.0"
 }
+# Resource group
+
+resource "azurerm_resource_group" "TEST" {
+name = "RG01"
+location = var.location
+
+# Virtual Network
+
+resource "azurerm_virtual_network" "TEST"{
+name					= "$(var.prefix)-vnet"
+address_space			= ["10.0.0.0/16"]
+location				= var.location
+resource_group_name		= azurerm_resource_group.TEST.name
+}
+
+# Sunet
+
+resource "azurerm_subnet" "TEST"{
+name					= "$(var.prefix)-subnet1"
+resource_group_name		= azurerm_resource_group.TEST.name
+virtual_network_name	= azurerm_virtual_network.TEST.name
+address_prefix			= ["10.0.2.0/24"]
+}
+
+#Public IP
+
+resource "azurerm_public_ip" "TEST"{
+name					= "tstPIP"
+resource_group_name		= azurerm_resource_group.TEST.name
+location				= var.location
+allocation_method		= "Static"
+}
+
+# NIC card
+
+resource "azurerm_network_interface" "TEST"{
+name					= "$(var.prefix)-NIC"
+location				= var.location
+resource_group_name		= azurerm_resource_group.TEST.name
+
+ip_configuration {
+
+name							= "internal"
+subnet_id						= azurerm_subnet.TEST.id
+private_ip_address_allocation	= "Dynamic"
+public_ip_address_id			= azurerm_public_ip.TEST.id 
+}
+}
+
+
+# Virtual Machine
+
+resource "azurerm_virtual_machine" "TEST"{
+name					= "$(var.prefix)-VM"
+location				= var.location
+resource_group_name		= azurerm_resource_group.TEST.name
+network_interface_ids	= [azurerm_network_interface.TEST.id]
+size					= "Standard_A1_V2"
+admin_username			= var.username
+admin_password			= var.password
+
+os_disk{
+ caching				= "ReadWrite"
+ storage_account_type	= "Standard_LRS"
+ }
+ 
+ source_image_reference {
+ 
+ publisher				= "MicrosoftWindowsServer"
+ offer					= "WindowsServer"
+ sku					= "2016-datacenter"
+ version				= "latest"
+ }
+ 
+
+delete_os_disk_on_termination	= TRUE
+delete_data_disk_on_termination	= TRUE
+
+}
+
+
+
+
+
+
